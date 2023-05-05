8b3dccbea49505fc2c2c9286fb0d124b993f15da
David Sarria <david.sarriav@gmail.com>
2023-05-05 23:12:49 +0200

    Primera API CRUD todos los paquetes

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
-const app = express();
 const cors = require('cors');
+const morgan = require('morgan');
+
+const app = express();
+
 
 const {
     handleGetAllData,
